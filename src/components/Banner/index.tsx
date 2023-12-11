a4767c8fb4f710f67bf0f2f39313897c6871ab57
Vinícios <vviniciosc@gmail.com>
2023-12-11 07:31:54 -0300

    :wrench: added Banner missing content

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import CodingFigure from "../../../public/coding-figure.svg";
+import LinkedinIcon from "../../../public/linkedin-icon.svg";
 
 const Banner = () => {
   return (
@@ -7,9 +8,12 @@ const Banner = () => {
       <div className="flex-grow basis-80 flex flex-col gap-5">
         <h1 className="font-black text-3xl xs:text-5xl text-center md:text-left bg-gradient-to-r from-primary-color to-accent-color text-transparent bg-clip-text">Desenvolvedor Front-End</h1>
         <p className="text-base xs:text-xl text-center md:text-left tracking-wide">
-          Olá! Me chamo <span className="font-black">Vinícios</span> e sou um <span className="underline">solucionador de problemas</span> apaixonado
-          por <span className="underline">tecnologia</span>.
+          Olá! Me chamo <span className="font-black">Vinícios Costa</span> e sou um <span className="underline underline-offset-4">solucionador de problemas</span> apaixonado por <span className="underline underline-offset-4">tecnologia</span>.
         </p>
+        <a href="https://www.linkedin.com/in/vinicioscst/" target="_blank"><button className="self-start flex items-center gap-2 font-bold text-sm py-2 px-4 border border-primary-color/90 bg-primary-color/90 text-background-color rounded-lg hover:opacity-75 hover:-translate-y-1 cursor-pointer transition-all">
+          <Image src={LinkedinIcon} alt="Ícone do Linkedin" title="Conecte-se comigo" width={16}/>
+          Conecte-se comigo
+        </button></a>
       </div>
       <Image
         src={CodingFigure}
