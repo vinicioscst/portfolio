54abeca301a779d40a4bae2b90e63fa37e818e0a
Vinícios <vviniciosc@gmail.com>
2023-12-11 05:02:38 -0300

    :new: added first components to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import Banner from "@/components/Banner";
+import Container from "@/components/Container";
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <></>
-  )
+    <>
+      <div className="h-1 bg-gradient-to-r from-primary-color to-accent-color"></div>
+      <Header />
+      <Container>
+        <main className="w-full my-20 flex flex-col gap-10">
+          <Banner />
+        </main>
+      </Container>
+    </>
+  );
 }
