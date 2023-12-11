d9a02e60708ed26bc5b2b87663f985bb1afdda0b
Vinícios <vviniciosc@gmail.com>
2023-12-11 07:32:35 -0300

    :new: added missing components to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import AboutMe from "@/components/AboutMe";
 import Banner from "@/components/Banner";
+import ContactSection from "@/components/ContactSection";
 import Container from "@/components/Container";
 import Header from "@/components/Header";
+import ProjectSection from "@/components/ProjectSection";
+import TechList from "@/components/TechsList";
 
 export default function Home() {
   return (
@@ -8,10 +12,15 @@ export default function Home() {
       <div className="h-1 bg-gradient-to-r from-primary-color to-accent-color"></div>
       <Header />
       <Container>
-        <main className="w-full my-20 flex flex-col gap-10">
+        <main className="w-full pt-20 pb-16 flex flex-col gap-20">
           <Banner />
+          <TechList />
+          <AboutMe />
+          <ProjectSection />
+          <ContactSection />
         </main>
       </Container>
+      <div className="h-1 bg-gradient-to-l from-primary-color to-accent-color"></div>
     </>
   );
 }
