e75d1f16da8b2985aba98561681cb5f578788f8d
Vinícios <vviniciosc@gmail.com>
2023-12-11 05:02:17 -0300

    :new: added link structure to nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,15 +3,23 @@ import Container from "../Container";
 const Header = () => {
   return (
     <Container>
-      <header className="flex flex-col flex-wrap xs:flex-row sm:justify-between justify-center items-center gap-4 py-5">
-        <span className="font-black text-2xl text-accent-color">
+      <header className="flex flex-col flex-wrap xs:flex-row sm:justify-between justify-center items-center gap-4 py-10">
+        <span className="font-black text-2xl text-accent-color select-none">
           &lt;vinícios /&gt;
         </span>
         <nav className="flex flex-wrap items-center gap-4 font-bold text-sm">
-          <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 rounded-lg hover:opacity-75 cursor-pointer transition-opacity">Sobre mim</li>
-          <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 rounded-lg hover:opacity-75 cursor-pointer transition-opacity">Projetos</li>
-          <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 rounded-lg hover:opacity-75 cursor-pointer transition-opacity">Contato</li>
-          <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 bg-primary-color/90 text-background-color rounded-lg hover:opacity-75 cursor-pointer transition-opacity">Baixar CV</li>
+          <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 rounded-lg hover:opacity-75 hover:-translate-y-1 cursor-pointer transition-all">
+            <a href="">Sobre mim</a>
+          </li>
+          <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 rounded-lg hover:opacity-75 hover:-translate-y-1 cursor-pointer transition-all">
+            <a href="">Projetos</a>
+          </li>
+          <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 rounded-lg hover:opacity-75 hover:-translate-y-1 cursor-pointer transition-all">
+            <a href="">Contato</a>
+          </li>
+          <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 bg-primary-color/90 text-background-color rounded-lg hover:opacity-75 hover:-translate-y-1 cursor-pointer transition-all">
+            <a href="/ViniciosCosta_Curriculo.pdf" download>Baixar CV</a>
+          </li>
         </nav>
       </header>
     </Container>
