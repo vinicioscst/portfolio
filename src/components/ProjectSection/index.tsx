211752ac59e18317863c8a82a6cce0a6254bd2ad
Vinícios <vviniciosc@gmail.com>
2023-12-11 16:05:44 -0300

    :wrench: fixed projects links

--- src/components/ProjectSection/index.tsx
+++ src/components/ProjectSection/index.tsx
@@ -3,10 +3,11 @@ import ProjectCard from "../ProjectCard";
 const ProjectSection = () => {
   const projects = [
     {
-      title: "KenzieHub",
-      description: "A KenzieHub é uma aplicação do tipo CRUD onde, além de incluir um sistema de cadastro e login, é possível listar as tecnologias que os alunos mais utilizam e o nível de conhecimento em cada uma delas.",
+      title: "Kenzie Hub",
+      description: "A Kenzie Hub é uma aplicação do tipo CRUD onde, além de incluir um sistema de cadastro e login, é possível listar as tecnologias que os alunos mais utilizam e o nível de conhecimento em cada uma delas.",
       techs: ["Javascript", "React", "Styled-Components"],
-      deploy: "https://react-entrega-kenzie-hub-vxnicios-8kldfucka-vxnicios.vercel.app/",
+      repository: "https://github.com/vinicioscst/kenzie-kenziehub",
+      deploy: "https://kenziehub-vinicioscst.vercel.app",
     },
     {
       title: "Weather App",
@@ -19,13 +20,15 @@ const ProjectSection = () => {
       title: "Nu Kenzie",
       description: "A Nu Kenzie é uma aplicação para controle financeiro, possibilitando inserir entradas e despesas, com seus respectivos valores e descrições.",
       techs: ["Javascript", "React", "Styled-Components"],
-      deploy: "https://react-entrega-s1-template-nu-kenzie-vxnicios.vercel.app",
+      repository: "https://github.com/vinicioscst/kenzie-nukenzie",
+      deploy: "https://nukenzie-vinicioscst.vercel.app/",
     },
     {
       title: "Git Search",
       description: "Uma simples aplicação para consultar perfis do Github, listando o nome do usuário, foto e seus repositórios disponibilizados publicamente.",
       techs: ["HTML", "CSS", "Javascript"],
-      deploy: "https://kenzie-academy-brasil-developers.github.io/gitSearchBase_vinicioscst/",
+      repository: "https://github.com/vinicioscst/kenzie-gitsearch",
+      deploy: "https://vinicioscst.github.io/kenzie-gitsearch/",
     },
   ];
 
