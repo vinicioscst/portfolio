68465eb054c719aca649e03c48ce672029e1c52a
Vinícios <vviniciosc@gmail.com>
2023-12-11 07:29:48 -0300

    :new: created TechsList component

---
+++ src/components/TechsList/index.tsx
@@ -0,0 +1,84 @@
+import Image from "next/image";
+import CSSIcon from "../../../public/css-icon.svg";
+import HTMLIcon from "../../../public/html-icon.svg";
+import JSIcon from "../../../public/javascript-icon.svg";
+import NextJSIcon from "../../../public/nextjs-icon.svg";
+import NodeJSIcon from "../../../public/nodejs-icon.svg";
+import PythonIcon from "../../../public/python-icon.svg";
+import ReactIcon from "../../../public/react-icon.svg";
+import TailwindIcon from "../../../public/tailwind-icon.svg";
+
+const TechList = () => {
+  return (
+    <section>
+      <ul className="overflow-x-auto flex justify-between gap-16 bg-[#101318] rounded-3xl py-5 px-8">
+        <Image
+          src={HTMLIcon}
+          alt="Ícone do HTML"
+          title="HTML"
+          width={35}
+          height={35}
+          className="opacity-75 hover:opacity-100 cursor-pointer transition-opacity"
+        />
+        <Image
+          src={CSSIcon}
+          alt="Ícone do CSS"
+          title="CSS"
+          width={35}
+          height={35}
+          className="opacity-75 hover:opacity-100 cursor-pointer transition-opacity"
+        />
+        <Image
+          src={TailwindIcon}
+          alt="Ícone do Tailwind"
+          title="Tailwind"
+          width={35}
+          height={35}
+          className="opacity-75 hover:opacity-100 cursor-pointer transition-opacity"
+        />
+        <Image
+          src={JSIcon}
+          alt="Ícone do Javascript"
+          title="Javascript"
+          width={35}
+          height={35}
+          className="opacity-75 hover:opacity-100 cursor-pointer transition-opacity"
+        />
+        <Image
+          src={ReactIcon}
+          alt="Ícone do React"
+          title="React"
+          width={35}
+          height={35}
+          className="opacity-75 hover:opacity-100 cursor-pointer transition-opacity"
+        />
+        <Image
+          src={NextJSIcon}
+          alt="Ícone do NextJS"
+          title="NextJS"
+          width={35}
+          height={35}
+          className="opacity-75 hover:opacity-100 cursor-pointer transition-opacity"
+        />
+        <Image
+          src={NodeJSIcon}
+          alt="Ícone do NodeJS"
+          title="NodeJS"
+          width={35}
+          height={35}
+          className="opacity-75 hover:opacity-100 cursor-pointer transition-opacity"
+        />
+        <Image
+          src={PythonIcon}
+          alt="Ícone do Python"
+          title="Python"
+          width={35}
+          height={35}
+          className="opacity-75 hover:opacity-100 cursor-pointer transition-opacity"
+        />
+      </ul>
+    </section>
+  );
+};
+
+export default TechList;
