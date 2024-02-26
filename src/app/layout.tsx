9c88bf415dc46580f46bfc0d418c6f51afd89cfb
“vinicioscst” <dev.vinicios@gmail.com>
2024-02-26 03:41:00 -0300

    style: fixed smooth scrolling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,15 @@ import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '700', '900'] })
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '700', '900'],
+})
 
 export const metadata: Metadata = {
   title: 'Vinícios Costa | Desenvolvedor Web',
-  description: 'Apaixonado por tecnologia e buscando minha primeira oportunidade no mercado de trabalho como desenvolvedor front-end, combinando com minha experiência em design para criar experiências digitais envolventes.',
+  description:
+    'Apaixonado por tecnologia e buscando minha primeira oportunidade no mercado de trabalho como desenvolvedor front-end, combinando com minha experiência em design para criar experiências digitais envolventes.',
 }
 
 export default function RootLayout({
@@ -15,8 +19,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-BR">
-      <body className={`${poppins.className} bg-background-color text-type-color scroll-smooth`}>{children}</body>
+    <html lang="pt-BR" className="scroll-smooth scroll-pt-8">
+      <body
+        className={`${poppins.className} bg-background-color text-type-color`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
