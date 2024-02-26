727b6fbdb89bc035c9d2146181ece06df735e152
“vinicioscst” <dev.vinicios@gmail.com>
2024-02-26 03:43:04 -0300

    fix: improved links behavior

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import Container from "../Container";
+import Container from '../Container'
 
 const Header = () => {
   return (
@@ -8,22 +8,30 @@ const Header = () => {
           &lt;vinícios /&gt;
         </span>
         <nav className="flex flex-wrap items-center gap-4 font-bold text-sm">
-          <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 rounded-lg hover:opacity-75 hover:-translate-y-1 cursor-pointer transition-all">
-            <a href="#about-me">Sobre mim</a>
-          </li>
-          <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 rounded-lg hover:opacity-75 hover:-translate-y-1 cursor-pointer transition-all">
-            <a href="#projects">Projetos</a>
-          </li>
-          <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 rounded-lg hover:opacity-75 hover:-translate-y-1 cursor-pointer transition-all">
-            <a href="#contact">Contato</a>
-          </li>
-          <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 bg-primary-color/90 text-background-color rounded-lg hover:opacity-75 hover:-translate-y-1 cursor-pointer transition-all">
-            <a href="/ViniciosCosta_Curriculo.pdf" download>Baixar CV</a>
-          </li>
+          <a href="#about-me">
+            <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 rounded-lg hover:opacity-75 hover:-translate-y-1 cursor-pointer transition-all">
+              Sobre mim
+            </li>
+          </a>
+          <a href="#projects">
+            <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 rounded-lg hover:opacity-75 hover:-translate-y-1 cursor-pointer transition-all">
+              Projetos
+            </li>
+          </a>
+          <a href="#contact">
+            <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 rounded-lg hover:opacity-75 hover:-translate-y-1 cursor-pointer transition-all">
+              Contato
+            </li>
+          </a>
+          <a href="/ViniciosCosta_Curriculo.pdf" download>
+            <li className="w-full xs:w-auto py-2 px-4 border border-primary-color/90 bg-primary-color/90 text-background-color rounded-lg hover:opacity-75 hover:-translate-y-1 cursor-pointer transition-all">
+              Baixar CV
+            </li>
+          </a>
         </nav>
       </header>
     </Container>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
