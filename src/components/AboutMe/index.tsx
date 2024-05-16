723659d73563fa7b59b99fba6bbedafbf4a2b73a
Vinícios Costa <dev.vinicios@gmail.com>
2024-05-16 10:50:58 -0300

    fix: make changes to eslint to enable deploy

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -5,7 +5,13 @@ const AboutMe = () => {
         Sobre mim
       </h2>
       <p className="text-sm xs:text-lg text-center md:text-left tracking-wide">
-        Estou atualmente <span className="underline underline-offset-4">trabalhando como desenvolvedor trainee em uma multinacional</span>, onde continuo a aprimorar minhas habilidades como full stack. No momento, estou estudando <span className="font-bold">C#</span> e <span className="font-bold">.NET</span>.
+        Estou atualmente{' '}
+        <span className="underline underline-offset-4">
+          trabalhando como desenvolvedor trainee em uma multinacional
+        </span>
+        , onde continuo a aprimorar minhas habilidades como full stack. No
+        momento, estou estudando <span className="font-bold">C#</span> e{' '}
+        <span className="font-bold">.NET</span>.
       </p>
     </section>
   )
